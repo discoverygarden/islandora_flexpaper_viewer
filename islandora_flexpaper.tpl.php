a125905ae4511c9cc28ec42bb8b1f9ecf2cd0aab
William Panting <willtp87@hotmail.com>
2012-04-23 15:17:09 -0300

    code style

--- islandora_flexpaper.tpl.php
+++ islandora_flexpaper.tpl.php
@@ -1,9 +1,14 @@
 <?php
 /**
-* Template file to make a FlexPaper viewer.
-* @param string flexpaper_path
-* @param string swf_url
-*/
+ * @file
+ *   This file is the template to show a flexpaper swf player
+ *
+ * Template file to make a FlexPaper viewer.
+ *
+ * @param string flexpaper_path
+ * @param string swf_url
+ */
+
 ?>
 <div id="flexpaper">
 <div id="viewer"></div>
