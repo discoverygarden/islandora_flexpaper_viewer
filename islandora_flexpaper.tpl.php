9bab1994391f5f21cbaf31fcd234b83b28f10326
Daniel Lamb <daniel-dgi@Packers.(none)>
2013-11-15 13:44:49 -0400

    Shuffled js out of tpl file and into its own js used by Drupal behavior.

--- islandora_flexpaper.tpl.php
+++ islandora_flexpaper.tpl.php
@@ -12,13 +12,4 @@
 ?>
 <div id="flexpaper">
 <div id="viewer"></div>
-<script type="text/javascript">
-var fp = new FlexPaperViewer(
-"<?php echo $flexpaper_path; ?>/FlexPaperViewer",
-"viewer", //ID selector for the div above
-{
-config : <?php echo drupal_to_js($flexpaper_config);?>
-}
-);
-</script>
 </div>
